5acd7bd72aa17c552b5f557d22f138cc265a20ec
Pawel Karkocki <pawel.karkocki@gmail.com>
2023-02-22 18:45:19 +0000

    useFetch custom hook creted to fetch data

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,28 +1,32 @@
-import { useEffect, useState } from "react";
-import "./TripList.css"
+import { useState } from "react";
+import "./TripList.css";
+import  {useFetch}  from "../hooks/useFetch";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/trips")
-      .then((response) => response.json())
-      .then((json) => setTrips(json));
-  }, []);
-
-  console.log(trips);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+  const { data: trips } = useFetch(url);
 
   return (
     <div className="trip-list">
       <h2>TRIP LIST</h2>
       <ul>
-        {trips.map((trip) => (
-          <li key ={trip.id}>
+        {trips && trips.map((trip) => (
+          <li key={trip.id}>
             <h3>{trip.title}</h3>
             <p>{trip.price}</p>
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          Eropean Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 }
